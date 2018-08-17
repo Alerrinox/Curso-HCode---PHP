d6238c784393bbfe6d7456aaf98e4851cb0f1344
Rafael Barros de Oliveira <rafaelbarros2007@gmail.com>
2018-08-17 15:06:12 -0300

    Seção organizadas por pastas

---
+++ Exemplo-01.php
@@ -0,0 +1,20 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php
+
+	$nome = "Rafael Oliveira";
+
+	echo "$nome <br/>";
+
+	/* var_dump mostra o tipo da variavel, quantidade de caracteres e o conteúdo da variável. */
+
+	var_dump("$nome");
+
+	
+?>
+</body>
+</html>
