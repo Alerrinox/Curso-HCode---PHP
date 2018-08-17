371c58865af21cc808d9b2e70ec3cd48bab1caeb
Rafael Barros de Oliveira <rafaelbarros2007@gmail.com>
2018-08-17 15:57:51 -0300

    Seção organizada por pasta

---
+++ Exemplo-01.php
@@ -0,0 +1,20 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php
+
+	$nome = "Rafael Oliveira";
+
+	echo "$nome <br/>";
+
+	/* var_dump mostra o tipo da variavel, quantidade de caracteres e o conteúdo da variável. */
+
+	var_dump("$nome");
+
+	/* var_dump da bug nas versões 5.6.31 e 7.0.23, imprime o diretorio da pasta junto com o resultado. */
+?>
+</body>
+</html>
