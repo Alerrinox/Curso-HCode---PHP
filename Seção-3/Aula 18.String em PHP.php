8a91a61dc152ff184d2369fef39655982dfbb36c
Rafael Barros de Oliveira <rafaelbarros2007@gmail.com>
2018-08-19 20:06:11 -0300

    Aspas simples e duplas

---
+++ Seção-3/Aula 18.String em PHP.php
@@ -0,0 +1,11 @@
+<?php
+	// Strings
+	
+	$a = 30;
+	// Aspas dupla - pode ser usado normalmente para concatenar variáveis e textos.
+	echo "número : $s";
+	// Aspas simples - é usada para interpulação de variáveis, ou seja, mostra o nome da variável e Não o conteúdo da variável. 
+	echo 'número : $s';
+	
+
+?>
