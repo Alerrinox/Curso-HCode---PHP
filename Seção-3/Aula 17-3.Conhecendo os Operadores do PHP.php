0cfea5ade144062468bb55ac6e3d647997c0c5cd
Rafael Barros de Oliveira <rafaelbarros2007@gmail.com>
2018-08-19 15:15:44 -0300

    Exemplo 3 - operadores matematicos

---
+++ Seção-3/Aula 17-3.Conhecendo os Operadores do PHP.php
@@ -0,0 +1,36 @@
+<?php
+
+	//Operadores matemáticos
+
+	$a = 45;
+	$b = 22;
+
+ 	echo "adição";
+	echo $a + $b;
+
+	echo "<BR/>";
+
+	echo "subtração";
+	echo $a - $b;
+
+	echo "<BR/>";
+
+	echo "multiplicação";
+	echo $a * $b;
+
+	echo "<BR/>";
+
+	echo "divisão";
+	echo $a / $b;
+
+	echo "<BR/>";
+
+	echo "modulo";
+	echo $a % $b;
+
+	echo "<BR/>";
+
+	echo "ao quadrado";
+	echo $a ** $b;
+
+?>
