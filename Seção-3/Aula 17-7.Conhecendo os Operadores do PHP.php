87453802a05b6a55200d74f2712500990134bd89
Rafael Barros de Oliveira <rafaelbarros2007@gmail.com>
2018-08-19 19:24:47 -0300

    incremento (++) e decrement0 (--)

---
+++ Seção-3/Aula 17-7.Conhecendo os Operadores do PHP.php
@@ -0,0 +1,11 @@
+<?php
+
+	// Operadores incremental (++)
+	$a = 10;
+	// Existe o pré incremento que vem na frente da expressão e soma mais 1 antes do começo da resolução (ex: ++$a).
+	echo ++$a;
+	// Existe o pro incremento que vem na atrás da expressão e soma mais 1 depois do termino da resolução (ex: $a++).
+	echo "<BR/>";
+
+	// Tem o incremento e o decremento. O decremento subtrai 1 usando a expressão (--). 
+?>
