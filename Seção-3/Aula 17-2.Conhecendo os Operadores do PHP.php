49bdcd7540134c8585bd39800efdd313f9ad6705
Rafael Barros de Oliveira <rafaelbarros2007@gmail.com>
2018-08-19 13:52:26 -0300

    Exmplo 2 - operador pra Incremento

--- Seção-3/Aula 17-2.Conhecendo os Operadores do PHP.php
+++ Seção-3/Aula 17-2.Conhecendo os Operadores do PHP.php
@@ -1,5 +1,72 @@
 <?php
+// Operdadores de atribuição
 
+	//Exemplo 1 soma
+	//Operdador de atribuição
+	$valorTotal_1 = 0;
 
+	//Operdador de incremento adicional(+);
+ 	$valorTotal_1 += 20;
+
+	$valorTotal_1 += 314;
+
+	echo "Operdador de incremento adicional(+), resultado final: ";
+	echo "$valorTotal_1 <BR/>";
+
+	//////////////////////////////////////////////////////////////////
+
+	//Exemplo 2 subtração
+	//Operdador de atribuição
+	$valorTotal_2 = 314;
+
+	//Operdador de incremento adicional(+);
+ 	$valorTotal_2 -= 50;
+
+	$valorTotal_2 -= 201;
+
+	echo "Operdador de incremento subtração(-), resultado final: ";
+	echo "$valorTotal_2 <BR/>";
+
+	///////////////////////////////////////////////////////////////////
+
+	//Exemplo 3 divisão
+	//Operdador de atribuição
+	$valorTotal_3 = 314;
+
+	//Operdador de incremento adicional(+);
+ 	$valorTotal_3 /= 2;
+
+	$valorTotal_3 /= 2;
+
+	echo "Operdador de incremento divisão(/), resultado final: ";
+	echo "$valorTotal_3 <BR/>";
+
+	///////////////////////////////////////////////////////////////////
+
+	//Exemplo 4 multiplicação
+	//Operdador de atribuição
+	$valorTotal_4 = 2;
+
+	//Operdador de incremento multiplicação(*);
+ 	$valorTotal_4 *= 20;
+
+	$valorTotal_4 *= 214;
+
+	echo "Operdador de incremento multiplicação(*), resultado final: ";
+	echo "$valorTotal_4 <BR/>";
+
+	////////////////////////////////////////////////////////////////////
+
+	//Exemplo 5 Modulos
+	//Operdador de atribuição
+	$valorTotal_5 = 314;
+
+	//Operdador de incremento Modulo(%);
+ 	$valorTotal_5 %= 13;
+
+	$valorTotal_5 %= 3;
+
+	echo "Operdador de incremento resto da divisão(%), resultado final: ";
+	echo "$valorTotal_5 <BR/>";
 
 ?>
