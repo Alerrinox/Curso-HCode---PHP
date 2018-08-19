ebf759a49a03392329a05aad9062a51edda9a9c5
Rafael Barros de Oliveira <rafaelbarros2007@gmail.com>
2018-08-19 18:06:32 -0300

    Comparador space ship

---
+++ Seção-3/Aula 17-5.Conhecendo os Operadores do PHP.php
@@ -0,0 +1,10 @@
+<?php 
+    // Comparador SpaceShip
+
+	$a = 44;
+	$b = 33;
+
+	// O spaceShip é usado para comparar duas expressões, e se o elemento da esquerda for maior vai retornar 1, se o elemento da direita for maior vai retornar -1, esos dois (a e b) forem iguais retorna 0.
+	var_dump($a <=> $b);
+
+ ?>
