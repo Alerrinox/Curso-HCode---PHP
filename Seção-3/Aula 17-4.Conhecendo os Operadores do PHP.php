361ea5a173d946c6ebeba873a4918bb691fb755f
Rafael Barros de Oliveira <rafaelbarros2007@gmail.com>
2018-08-19 15:48:15 -0300

    Exemplo 4 - Comparadores lógicos

---
+++ Seção-3/Aula 17-4.Conhecendo os Operadores do PHP.php
@@ -0,0 +1,28 @@
+<?php 
+
+	$a = 55.0;
+	$b = 55;
+
+	//comparação se "a" é maior que "b"
+	var_dump($a > $b);
+
+	echo "<BR/>";
+	//comparação se "b" é menos que "a" 
+	var_dump($a < $b);
+
+	echo "<BR/>";
+	//comparação se o valor de "a" é igual a de "b"
+	var_dump($a == $b);
+
+	echo "<BR/>";
+	//comparação se o valor e o tipo  de "a" é igual a de "b"
+	var_dump($a === $b);
+
+	echo "<BR/>";
+	//comparação se o valor de "a" é igual a de "b"
+	var_dump($a != $b);
+
+
+
+
+ ?>
