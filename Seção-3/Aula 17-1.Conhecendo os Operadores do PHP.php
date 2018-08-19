5490242503babe5b54119969bb8aaa827b7a11e8
Rafael Barros de Oliveira <rafaelbarros2007@gmail.com>
2018-08-19 13:16:24 -0300

    Exemplo 1 - Operadores de String (atribuição e concatenação)

---
+++ Seção-3/Aula 17-1.Conhecendo os Operadores do PHP.php
@@ -0,0 +1,16 @@
+<?php 
+	////Conhecendo os Operadores do PHP
+
+	//Operador de atribuição
+	$Curso = "HCode";
+
+	//Operador de concatenação
+	echo $Curso ." mais alguma coisa <BR/><BR/>";
+
+	//Operador de incremento (usando concatenação com atribuição)
+	$Curso .= " Treinamentos";
+
+	//resultado
+	echo $Curso;
+
+ ?>
